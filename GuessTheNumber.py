040014b9a60ebc475a8e013c648020d2a3c5794d
Chung Do <daibanglua1296@gmail.com>
2021-01-24 15:43:26 +0700

    1st program

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,4 +1,17 @@
+import random
 
 print('I am thinking of a number between 1 and 20.')
-print('Take a guess.')
-
+res = random.randint(1, 21)
+guessCount = 0
+while True:
+    print("Take a guess.")
+    num = int(input())
+    if num < res:
+        print("Your guess is too low.")
+        guessCount += 1
+    elif num > res:
+        print("Your guess is too high.")
+        guessCount += 1
+    else:
+        print("Good job! You guessed my number in " + str(guessCount) + " guesses!")
+        break
